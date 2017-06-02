8ad6ed28d959f70310fbf0d05d6d1bcd881574a9
Brent Shepherd <thenbrent@gmail.com>
2017-06-02 11:15:06 -0700

    Add license

--- woocommerce-subscriptions-renewal-logger.php
+++ woocommerce-subscriptions-renewal-logger.php
@@ -1,12 +1,36 @@
 <?php
-/*
-Plugin Name: WooCommerce Subscriptions - Renewal Logger
-Plugin URI: https://github.com/Prospress/woocommerce-subscriptions-renewal-logger
-Description: Determine why automatic subscription renewal payments aren't being processed despite using an automatic gateway. Logs important data around renewal events to WooCommerce log file prefixed with 'wcs-renewal-log-'.
-Author: Prospress, James Allan
-Author URI:
-Version: 1.0
-*/
+/**
+ * Plugin Name: WooCommerce Subscriptions - Renewal Logger
+ * Plugin URI: https://github.com/Prospress/woocommerce-subscriptions-renewal-logger
+ * Description: Determine why automatic subscription renewal payments aren't being processed despite using an automatic gateway. Logs important data around renewal events to WooCommerce log file prefixed with 'wcs-renewal-log-'.
+ * Version: 1.0
+ * Author: Prospress Inc, James Allan
+ * Author URI: http://prospress.com
+ * License: GPLv3
+ *
+ * GitHub Plugin URI: Prospress/woocommerce-subscriptions-importer-exporter
+ * GitHub Branch: master
+ *
+ * Copyright 2017 Prospress, Inc.  (email : freedoms@prospress.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package		WooCommerce Subscriptions Renewal Logger
+ * @author		Prospress Inc.
+ * @since		1.0
+ */
+
 class WCS_Renewal_Logger {
 
 	private static $logger = null;
